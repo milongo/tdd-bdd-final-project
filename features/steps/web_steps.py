6a2cc221c00f15bbb3e018c84520167457c3dd05
Emilio <16996940+milongo@users.noreply.github.com>
2024-03-22 17:30:07 -0400

    simpler step

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -112,18 +112,13 @@ def step_impl(context, button):
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
-    found = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.text_to_be_present_in_element(
-            (By.ID, 'search_results'),
-            name
-        )
-    )
-    assert(found)
+    element = context.driver.find_element_by_id('search_results')
+    assert name in element.text
 
 @then('I should not see "{name}" in the results')
 def step_impl(context, name):
     element = context.driver.find_element_by_id('search_results')
-    assert(name not in element.text)
+    assert name not in element.text
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
